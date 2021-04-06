7863a0163bcf47f73618cb714e6a370cd7de2633
Vishnu <vishnu.brdvl@gmail.com>
2021-04-06 22:05:03 +1000

    added basic homescreen

--- SourceCode_and_Documentation/plus_one/lib/main.dart
+++ SourceCode_and_Documentation/plus_one/lib/main.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:plus_one/src/pages/loading_page.dart';
+import 'package:provider/provider.dart';
+
+import './src/providers/events.dart';
+import './src/providers/paths.dart';
+import './src/pages/homescreen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // apparently this only works on android ? https://tinyurl.com/dylpcq85
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
-    return MaterialApp(
-      home: LoadingPage(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: EventsList(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Paths(),
+        ),
+      ],
+      child: MaterialApp(
+        title: "PlusOne",
+        theme: ThemeData(
+          primarySwatch: Colors.grey,
+          accentColor: Colors.orange,
+        ),
+        home: HomeScreen(),
+      ),
     );
   }
 }
