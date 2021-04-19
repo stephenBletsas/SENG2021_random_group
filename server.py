c28bc8a645b30a78bcad0303181fed0647295430
Thiagaut Valcke <thiagautthithivalcke@icloud.com>
2021-04-19 12:57:56 +1000

    finished implementing ticketmaster API

--- server.py
+++ server.py
@@ -23,53 +23,36 @@ CORS(APP)
 APP.config['TRAP_HTTP_EXCEPTIONS'] = True
 APP.register_error_handler(Exception, defaultHandler)
 
-
-
-# def get_events(token="WIMXDPIS3V2F74GY3G7V"):
-#     # set headers with user token
-#     header = {"Authorization": "Bearer {}".format(token)}
-#     # hit API and parse response
-#     request = requests.get("https://www.eventbriteapi.com/v3/users/me/events/", headers=headers)
-#     response = request.json()
-#     our_events = []
-#     for event in response['events']:
-
-# $.ajax({
-#   type:"GET",
-#   url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey={apikey}",
-#   async:true,
-#   dataType: "json",
-#   success: function(json) {
-#               console.log(json);
-#               // Parse the response.
-#               // Do other things.
-#            },
-#   error: function(xhr, status, err) {
-#               // This time, we do not end up here!
-#            }
-# });
-
-#def func():
-#    apikey = "QifuffoT98eP9eJcC08aMGfjvjb9DXGp"
-#    req = request.get("https://app.ticketmaster.com/discovery/v2/events.json?startDateTime='19 september 2022'&apikey={apikey}")
-#    response = req.json()
-#    return response
-
-
 @APP.route('/hello', methods=['GET'])
 def hello():
     return "hello world"
 
+#eventbrite API {datetime, image, location, description}
 @APP.route('/event', methods=['GET'])
 def event():
-    req = requests.get("https://app.ticketmaster.com/discovery/v2/events?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&locale=*&startDateTime=2021-04-20T22:38:00Z&city=Sydney&countryCode=AU&stateCode=NSW")
+    data = request.args.get('datetime')
+    #datetime format 2021-04-20T22:38:00Z
+    data2 = f'{data[:4]}-{data[5:7]}-{data[8:10]}T23:59:59Z'
+    req = requests.get(f'https://app.ticketmaster.com/discovery/v2/events?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&locale=*&startDateTime={data}&endDateTime={data2}&city=Sydney&countryCode=AU&stateCode=NSW')
     response = req.json()
     our_events = []
-    for event in response["_embedded"]["events"]:
-        #for event2 in event["events"]:
-        our_events.append(event["name"])
+    try:
+        for event in response["_embedded"]["events"]:
+            my_dict = dict()
+            my_dict['name'] = event["name"]
+            my_dict['location'] = event["_embedded"]["venues"][0]["name"]
+            my_dict['image'] = event["images"][0]["url"]
+            my_dict['datetime'] = event["dates"]["start"]["dateTime"]
+            my_dict['description'] = event["info"]
+            our_events.append(my_dict)
+    except:
+        #no events found
+        our_events = []
     return dumps(our_events)
 
+#run app like this:
+#FLASK_APP=server.py flask run
 
+#the following is depricated
 #if __name__ == "__main__":
 #    APP.run()
