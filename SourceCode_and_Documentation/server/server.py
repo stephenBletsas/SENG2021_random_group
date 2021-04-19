046d0db18c49f7a3887e64de849675996a7443a7
Rushil Pardasani <rushil2001@hotmail.com>
2021-04-19 16:20:39 +0530

    Mixed code

--- SourceCode_and_Documentation/server/server.py
+++ SourceCode_and_Documentation/server/server.py
@@ -24,6 +24,27 @@ CORS(APP)
 APP.config['TRAP_HTTP_EXCEPTIONS'] = True
 APP.register_error_handler(Exception, defaultHandler)
 
+@APP.route('/event', methods=['GET'])
+def event():
+    data = request.args.get('datetime')
+    #datetime format 2021-04-20T22:38:00Z
+    data2 = f'{data[:4]}-{data[5:7]}-{data[8:10]}T23:59:59Z'
+    req = requests.get(f'https://app.ticketmaster.com/discovery/v2/events?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&locale=*&startDateTime={data}&endDateTime={data2}&city=Sydney&countryCode=AU&stateCode=NSW')
+    response = req.json()
+    our_events = []
+    try:
+        for event in response["_embedded"]["events"]:
+            my_dict = dict()
+            my_dict['name'] = event["name"]
+            my_dict['location'] = event["_embedded"]["venues"][0]["name"]
+            my_dict['image'] = event["images"][0]["url"]
+            my_dict['datetime'] = event["dates"]["start"]["dateTime"]
+            my_dict['description'] = event["info"]
+            our_events.append(my_dict)
+    except:
+        #no events found
+        our_events = []
+    return dumps(our_events)
 
 @APP.route('/restaurant', methods=['GET'])
 def restaurant():
