716652b377536e67cb0852cf495962330d10ddcb
Vishnu <vishnu.brdvl@gmail.com>
2021-04-20 16:35:46 +1000

    configured firebase for ios

--- SourceCode_and_Documentation/plus_one/lib/src/pages/chats/chat_page.dart
+++ SourceCode_and_Documentation/plus_one/lib/src/pages/chats/chat_page.dart
@@ -1,44 +1,43 @@
-import 'package:flutter/material.dart';
-import 'package:plus_one/src/styling/color_palettes.dart';
+// import 'package:flutter/material.dart';
+// import 'package:plus_one/src/styling/color_palettes.dart';
 
+// class ChatPage extends StatefulWidget {
+//   @override
+//   _ChatPageState createState() => _ChatPageState();
+// }
 
-class ChatPage extends StatefulWidget {
-  @override
-  _ChatPageState createState() => _ChatPageState();
-}
-
-class _ChatPageState extends State<ChatPage>{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: sixtyPercOrange,
-        title: Center(
-            child: Text(
-          'PlusOne',
-          style: TextStyle(
-              color: unselectedGray, fontWeight: FontWeight.bold, fontSize: 25),
-        )),
-        leading: IconButton(
-            icon: Icon(
-              Icons.arrow_back_ios,
-              color: unselectedGray,
-            ),
-            onPressed: () {
-              Navigator.pop(context);
-            }),
-        actions: [
-          Icon(
-            Icons.call,
-            color: Colors.transparent,
-          ),
-          Icon(
-            Icons.call,
-            color: Colors.transparent,
-          )
-        ],
-      ),
-      body: Text("CHAT PAGE"),
-      );
-  }
-}
+// class _ChatPageState extends State<ChatPage>{
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: sixtyPercOrange,
+//         title: Center(
+//             child: Text(
+//           'PlusOne',
+//           style: TextStyle(
+//               color: unselectedGray, fontWeight: FontWeight.bold, fontSize: 25),
+//         )),
+//         leading: IconButton(
+//             icon: Icon(
+//               Icons.arrow_back_ios,
+//               color: unselectedGray,
+//             ),
+//             onPressed: () {
+//               Navigator.pop(context);
+//             }),
+//         actions: [
+//           Icon(
+//             Icons.call,
+//             color: Colors.transparent,
+//           ),
+//           Icon(
+//             Icons.call,
+//             color: Colors.transparent,
+//           )
+//         ],
+//       ),
+//       body: Text("CHAT PAGE"),
+//       );
+//   }
+// }
